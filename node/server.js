fb36fda9ae29977a309c379b4a6ec2fde3ec8993
Adam <jianming_221@yahoo.com>
2017-12-17 23:04:53 +0800

    1

---
+++ node/server.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var server = http.createServer(function(req,res){
+    res.writeHead(200, {'Content_Type': 'text/plain'});
+    res.end('hello world');
+})
+server.listen(8888);
+console.log('server is running @port:8888');
